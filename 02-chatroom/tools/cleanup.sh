ea98606f5f9ed3eb9ab5d1fa25ff2300fb302492
kasicass <kasicass@gmail.com>
2026-06-02 21:10:42 +0800

    02-chatroom init

---
+++ 02-chatroom/tools/cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# # step 1: rm -rf subdirectories
+# for d in */; do
+#     rm -rf "$d"
+# done
+
+# # step 2: rm files except bootstrap.sh, cleanup.sh
+# for f in *; do
+#     if [ -f "$f" ] && [ "$f" != "bootstrap.sh" ] && [ "$f" != "cleanup.sh" ]; then
+#         rm "$f"
+#     fi
+# done
+
+rm lua
+rm skynet
